ee6817085867f5335aa33bccdf57124c66bf59b5
Bruno Peck Marques <brunopeckmarques@gmail.com>
2021-05-26 20:45:03 +0200

    loading screen and error handling for planets

--- src/main/webapp/planets/src/Planets/index.tsx
+++ src/main/webapp/planets/src/Planets/index.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 
+import { AxiosError, AxiosResponse } from "axios";
+
 import { makeStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
@@ -8,6 +10,9 @@ import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Snackbar from "@material-ui/core/Snackbar";
+import MuiAlert from "@material-ui/lab/Alert";
 
 import { PagedResults, Planet } from "../models";
 import axios from "../axios";
@@ -25,36 +30,83 @@ const Planets: React.FunctionComponent<{}> = () => {
     previous: null,
     results: [],
   });
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>("");
 
   const classes = useStyles();
 
   useEffect(() => {
+    setIsLoading(true);
     axios({ method: "GET" })
-      .then((res) => setRowData(res.data))
-      .catch(console.error);
+      .then((res: AxiosResponse<PagedResults<Planet>>) => {
+        setRowData(res.data);
+        setError("");
+        setIsLoading(false);
+      })
+      .catch((err: AxiosError) => {
+        setIsLoading(false);
+        setError(err.message);
+        setInterval(() => setError(""), 5000);
+      });
   }, []);
+
+  const hasError: boolean = !isLoading && Boolean(error);
+
+  if (hasError) {
+    return (
+      <Snackbar
+        anchorOrigin={{ vertical: "top", horizontal: "center" }}
+        open={hasError}
+        message={error}
+        key="top center"
+      >
+        <MuiAlert elevation={6} variant="filled" severity="error">
+          {error}
+        </MuiAlert>
+      </Snackbar>
+    );
+  }
+
+  let bodyToRender: JSX.Element | JSX.Element[];
+  if (isLoading) {
+    bodyToRender = (
+      <TableRow>
+        <TableCell colSpan={4} align="center">
+          <CircularProgress />
+        </TableCell>
+      </TableRow>
+    );
+  } else if (!isLoading && rowData.count > 0) {
+    bodyToRender = rowData.results.map((data: Planet) => (
+      <TableRow key={data.name}>
+        <TableCell align="center">{data.name}</TableCell>
+        <TableCell align="center">{data.climate}</TableCell>
+        <TableCell align="center">{data.terrain}</TableCell>
+        <TableCell align="center">{data.created}</TableCell>
+      </TableRow>
+    ));
+  } else {
+    bodyToRender = (
+      <TableRow>
+        <TableCell colSpan={4} align="center">
+          Data not found.
+        </TableCell>
+      </TableRow>
+    );
+  }
 
   return (
     <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="simple table">
+      <Table className={classes.table}>
         <TableHead>
           <TableRow>
-            <TableCell align="right">Name</TableCell>
-            <TableCell align="right">Climate</TableCell>
-            <TableCell align="right">Terrain</TableCell>
-            <TableCell align="right">Created</TableCell>
+            <TableCell align="center">Name</TableCell>
+            <TableCell align="center">Climate</TableCell>
+            <TableCell align="center">Terrain</TableCell>
+            <TableCell align="center">Created</TableCell>
           </TableRow>
         </TableHead>
-        <TableBody>
-          {rowData.results.map((row) => (
-            <TableRow key={row.name}>
-              <TableCell align="right">{row.name}</TableCell>
-              <TableCell align="right">{row.climate}</TableCell>
-              <TableCell align="right">{row.terrain}</TableCell>
-              <TableCell align="right">{row.created}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
+        <TableBody>{bodyToRender}</TableBody>
       </Table>
     </TableContainer>
   );
--- src/main/webapp/planets/src/axios.ts
+++ src/main/webapp/planets/src/axios.ts
@@ -1,3 +1,3 @@
 import axios from "axios";
 
-export default axios.create({ baseURL: "http://localhost:8080/api/planets" });
+export default axios.create({ baseURL: "http://localhost:8080/api/planets/" });
