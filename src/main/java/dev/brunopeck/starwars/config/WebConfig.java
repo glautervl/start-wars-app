2b3ba1295a4a9b78c35b14d0901c7c3eb4c9570b
Bruno Peck Marques <brunopeckmarques@gmail.com>
2021-05-27 08:34:19 +0200

    fix refresh browser returning 404 if not on root path

--- src/main/java/dev/brunopeck/starwars/config/WebConfig.java
+++ src/main/java/dev/brunopeck/starwars/config/WebConfig.java
@@ -2,8 +2,12 @@ package dev.brunopeck.starwars.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+import java.io.IOException;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -11,12 +15,15 @@ public class WebConfig implements WebMvcConfigurer {
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/**/*")
 			.addResourceLocations("classpath:/public/")
-			.resourceChain(false)
-			.addTransformer((httpServletRequest, resource, resourceTransformerChain) -> {
-				if (resource.exists() && resource.isReadable()) {
-					return resource;
+			.resourceChain(true)
+			.addResolver(new PathResourceResolver() {
+				@Override
+				protected Resource getResource(String resourcePath, Resource location) throws IOException {
+					Resource requestedResource = location.createRelative(resourcePath);
+					return requestedResource.exists() && requestedResource.isReadable()
+							? requestedResource
+							: new ClassPathResource("/public/index.html");
 				}
-				return new ClassPathResource("index.html");
 			});
 	}
 }
